b55717e8acbb258dae397c8cefb815200d0f1166
OXIGEN1UM <chembrid9e@gmail.com>
2018-03-01 22:33:01 +0300

    file path server

--- path/server.js
+++ path/server.js
@@ -1,4 +1,38 @@
-function sendFileSafe(path, res) {
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const root = __dirname + '/public';
+
+http.createServer(function(req, res) {
+
+  if (checkAccess(req)) {
+    res.statusCode = 403;
+    res.end("Tell me the key!");
+    return;
+  }
+
+  sendFileSafe(url.parse(req.url).pathname, res);
+
+}).listen(8080);
+
+function checkAccess(req) {
+  return url.parse(req.url, true).query.secret == '0_o';
+}
+
+function sendFileSafe(filePath, res) {
+  try {
+  filePath = decodeURIComponent(filePath); // decoding of incoming url
+  } catch(e) {
+    res.statusCode = 400;
+    res.end("Bad reqest");
+    return;
+  }
+
+}
+
+function sendFileSafe(filePath, res) {
 
   try {
   filePath = decodeURIComponent(filePath); // decoding of url if needed
